5aa98afb3a5079c9be62f41a704f17b37dc5cb54
geoffrey.floyd <geoffrey.floyd@hoomanlogic.com>
2015-05-06 12:31:04 -0400

    refactor notifications

---
+++ src/client/jsx/components/NotificationList.jsx
@@ -0,0 +1,111 @@
+/** @jsx React.DOM */
+
+// CommonJS, AMD, and Global shim
+(function (root, factory) {
+    'use strict';
+	if (typeof exports === "object") {
+		// CommonJS
+		module.exports = exports = factory(
+            require('react'),
+            require('../../js/stores/NotificationStore'),
+            require('../../../../../react_components/src/DropDownMenu'),
+            require('./NotificationListItem')
+        );
+	}
+	else if (typeof define === "function" && define.amd) {
+		// AMD
+		define([
+            'react',
+            '../../js/stores/NotificationStore',
+            '../../../../../react_components/src/DropDownMenu',
+            './NotificationListItem'
+        ], factory);
+	}
+	else {
+		// Global (browser)
+		root.NotificationList = factory(
+            root.React,
+            root.notificationStore,
+            root.NotificationListItem
+        );
+	}
+}(this, function (React, notificationStore, NotificationListItem) {
+    'use strict';
+    return React.createClass({
+        /*************************************************************
+         * EVENT HANDLING
+         *************************************************************/
+        handleNotificationClicked: function (notification, verb) {
+            if (!_.isUndefined(verb) && verb === 'Accept Connection') {
+                connectionStore.acceptConnection(notification.subject);
+            } else if (!_.isUndefined(verb) && verb === 'Reject Connection') {
+                connectionStore.rejectConnection(notification.subject);
+            } else {
+                if (notification.kind === 'New Message') {
+                    ui.openConversation(notification.subject);
+                } else if (notification.kind === 'Connection Accepted') {
+                    ui.goTo('Connect');
+                }
+                notificationStore.acknowledgeNotification(notification);
+            }
+        },
+
+        /*************************************************************
+         * RENDERING
+         *************************************************************/
+        render: function () {
+            var domUnread,
+                domRead;
+            
+            var notifications = _.sortBy(notificationStore.updates.value, 'occurredAt').reverse();
+            
+            var unreadNotifications = _.where(notifications, {readAt: null});
+            
+            var readNotifications = _.filter(notifications, function (item) { 
+                return typeof item.readAt !== 'undefined' && item.readAt !== null;
+            });
+            
+            if (unreadNotifications.length > 0) {
+                domUnread = (
+                    <div>
+                        <ul className="notifications">
+                            {unreadNotifications.map( 
+                                function(item) {
+                                    return (<NotificationListItem key={item.id} data={item} handleNotificationClicked={this.handleNotificationClicked} />);
+                                }.bind(this)
+                            )}
+                        </ul>
+                    </div>
+                );
+            } else {
+                domUnread = (
+                    <div>
+                        <h2 style={{margin: '0.2rem'}}>No new notifications. You're all caught up!</h2>
+                    </div>
+                );
+            }
+            
+            if (readNotifications.length > 0) {
+                domRead = (
+                    <div>
+                        <h2 style={{margin: '0.2rem'}}>Recent Notifications</h2>
+                        <ul className="notifications">
+                            {readNotifications.map( 
+                                function(item) {
+                                    return (<NotificationListItem key={item.id} data={item} handleNotificationClicked={this.handleNotificationClicked} />);
+                                }.bind(this)
+                            )}
+                        </ul>
+                    </div>
+                );
+            }
+            
+            return (
+                <div style={{padding: '5px'}}>
+                    {domUnread}
+                    {domRead}
+                </div>
+            );
+        }
+    });
+}));
--- src/client/jsx/components/NotificationListItem.jsx
+++ src/client/jsx/components/NotificationListItem.jsx
@@ -1,48 +1,74 @@
-var NotificationListItem = React.createClass({
-    /*************************************************************
-     * RENDERING
-     *************************************************************/
-    render: function () {
-        var item = this.props.data;
-        
-        var classNames = 'notification';
-        if (item.readAt !== null) {
-            classNames += ' read';   
-        }
-        var notification = null;
-        if (item.kind === 'Connection Request') {
-            notification = (
-                <a onClick={this.props.handleNotificationClicked.bind(null, item)}>
-                    <span className={classNames}>{item.subject} requested to connect with you</span>
-                    <button type="button" className="btn btn-success pull-right" onClick={this.props.handleNotificationClicked.bind(null, item, 'Accept Connection')}><i className="fa fa-check-square-o"></i></button>
-                    <button type="button" className="btn btn-danger pull-right" onClick={this.props.handleNotificationClicked.bind(null, item, 'Reject Connection')}><i className="fa fa-trash"></i></button>
-                </a>  
-            );
-        } else if (item.kind === 'Connection Accepted') {
-            notification = (
-                <a onClick={this.props.handleNotificationClicked.bind(null, item)}>
-                    <span className={classNames}>{item.subject} accepted your connection request</span>
-                </a>  
-            );
-        } else if (item.kind === 'New Message') {
-            notification = (
-                <a onClick={this.props.handleNotificationClicked.bind(null, item)}>
-                    <span className={classNames}>{item.subject} sent you a message</span>
-                </a>  
-            );
-        } else {
-            // general notifications
-            notification = (
-                <a onClick={this.props.handleNotificationClicked.bind(null, item)}>
-                    <span className={classNames}>{item.subject}</span>
-                </a>
-            );
-        }
+/** @jsx React.DOM */
 
-        return (
-            <li className="notification" key={'notify' + item.occurredAt.getTime()}>
-                {notification}
-            </li>
+// CommonJS, AMD, and Global shim
+(function (root, factory) {
+    'use strict';
+	if (typeof exports === "object") {
+		// CommonJS
+		module.exports = exports = factory(
+            require('react')
         );
-    }
-});
+	}
+	else if (typeof define === "function" && define.amd) {
+		// AMD
+		define([
+            'react'
+        ], factory);
+	}
+	else {
+		// Global (browser)
+		root.NotificationListItem = factory(
+            root.React
+        );
+	}
+}(this, function (React, actionStore, TimerBar, WeatherIcon, FocusActions, ManageFocus, AddEditAction, LogAction, Conversation) {
+    'use strict';
+    return React.createClass({
+        /*************************************************************
+         * RENDERING
+         *************************************************************/
+        render: function () {
+            var item = this.props.data;
+
+            var classNames = 'notification';
+            if (item.readAt !== null) {
+                classNames += ' read';   
+            }
+            var notification = null;
+            if (item.kind === 'Connection Request') {
+                notification = (
+                    <a onClick={this.props.handleNotificationClicked.bind(null, item)}>
+                        <span className={classNames}>{item.subject} requested to connect with you</span>
+                        <button type="button" className="btn btn-success pull-right" onClick={this.props.handleNotificationClicked.bind(null, item, 'Accept Connection')}><i className="fa fa-check-square-o"></i></button>
+                        <button type="button" className="btn btn-danger pull-right" onClick={this.props.handleNotificationClicked.bind(null, item, 'Reject Connection')}><i className="fa fa-trash"></i></button>
+                    </a>  
+                );
+            } else if (item.kind === 'Connection Accepted') {
+                notification = (
+                    <a onClick={this.props.handleNotificationClicked.bind(null, item)}>
+                        <span className={classNames}>{item.subject} accepted your connection request</span>
+                    </a>  
+                );
+            } else if (item.kind === 'New Message') {
+                notification = (
+                    <a onClick={this.props.handleNotificationClicked.bind(null, item)}>
+                        <span className={classNames}>{item.subject} sent you a message</span>
+                    </a>  
+                );
+            } else {
+                // general notifications
+                notification = (
+                    <a onClick={this.props.handleNotificationClicked.bind(null, item)}>
+                        <span className={classNames}>{item.subject}</span>
+                    </a>
+                );
+            }
+
+            return (
+                <li className="notification" key={'notify' + item.occurredAt.getTime()}>
+                    {notification}
+                </li>
+            );
+        }
+    });
+}));
